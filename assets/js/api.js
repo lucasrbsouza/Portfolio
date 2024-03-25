be2f9f37ce38aff422a4ca108381d12f2108b3e6
lucasrbsouza <joselucastrbssilvasouza@gmail.com>
2024-03-25 11:11:48 -0300

    ajustando api

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 async function fetchProfileData() {
-    const url = '/data/profile.json';
-    //const url = 'https://raw.githubusercontent.com/lucasrbsouza/Portfolio/main/Data/profile.json?token=GHSAT0AAAAAACPUSKTK2S2YYJP5TEOJNKGIZQATZRQ '
+    //const url = '/data/profile.json';
+    const url = 'https://raw.githubusercontent.com/lucasrbsouza/Portfolio/main/Data/profile.json'
     const fetching= await fetch(url)
     return await fetching.json()
     
